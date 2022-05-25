66f98d98a43d1069bd50d8fc203fb05a1652fbe0
Mohammad Jaffal <mhmmdjaffal@gmail.com>
2022-05-25 22:06:51 +0300

    Initializing the project

---
+++ JS/script-sheet.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+
+
+
+    
+
+
+    $('#add').click(function () {
+
+        var title = $('#title').val();
+        var description = $('#description').val();
+        // console.log(title);
+        // console.log(description);
+        if (title == "" || description == "")
+            console.log("nope");
+        else {
+
+
+
+        }
+
+    });
+
+
+
+
+
+
+
+
+});
